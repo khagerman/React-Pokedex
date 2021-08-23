76102597f80e853aac64a42e84a26e68ce74299d
khagerman <71734063+khagerman@users.noreply.github.com>
2021-08-23 14:39:59 -0500

    general css cleanup and fixed warnings

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,12 +1,18 @@
+import React from "react";
+
+import "./Pokecard.css";
 function Pokecard({ name, id, type, exp }) {
   return (
     <div className="Pokecard">
       <h3 className="Pokecard-title">{name}</h3>
       <img
         src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}
+        alt="pokemon"
       />
-      <p>Type:{type}</p>
+      <p>Type: {type}</p>
       <p>EXP:{exp}</p>
     </div>
   );
 }
+
+export default Pokecard;
